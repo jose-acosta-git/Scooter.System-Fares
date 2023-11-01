941d08ae9453a5c37856eaf67143e5c3c423f8c2
jose-acosta-git <jose.acosta.git@gmail.com>
2023-11-01 17:07:25 -0300

    Add base structure of SpringBoot app

---
+++ src/main/java/fares/FaresApplication.java
@@ -0,0 +1,13 @@
+package fares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fares/FaresApplicationTests.java
@@ -0,0 +1,13 @@
+package fares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
